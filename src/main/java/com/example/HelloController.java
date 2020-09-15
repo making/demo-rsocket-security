8eb5e6c7e437c4bca264a33efff1873f8843b003
Toshiaki Maki <tmaki@pivotal.io>
2020-09-15 23:21:30 +0900

    Cleanup

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,15 +1,8 @@
 package com.example;
 
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.context.annotation.Profile;
-import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.rsocket.annotation.ConnectMapping;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -17,18 +10,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 @Profile("!bearer")
 public class HelloController {
-	private static final Logger log = LoggerFactory.getLogger(HelloController.class);
-
-	@ConnectMapping
-	public void setup(@Payload String payload, @Headers Map<String, Object> headers) {
-		log.info("setup:payload={}", payload);
-		log.info("setup:headers={}", headers);
-	}
 
 	@MessageMapping("hello")
-	public String hello(@AuthenticationPrincipal UserDetails user, @Payload String payload, @Headers Map<String, Object> headers) {
-		log.info("hello:payload={}", payload);
-		log.info("hello:headers={}", headers);
+	public String hello(@AuthenticationPrincipal UserDetails user, @Payload String payload) {
 		return "Hello " + payload + ", " + user.getUsername() + "!";
 	}
 }
--- src/main/java/com/example/HelloControllerBearer.java
+++ src/main/java/com/example/HelloControllerBearer.java
@@ -1,15 +1,8 @@
 package com.example;
 
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.context.annotation.Profile;
-import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.rsocket.annotation.ConnectMapping;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Controller;
@@ -17,18 +10,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 @Profile("bearer")
 public class HelloControllerBearer {
-	private static final Logger log = LoggerFactory.getLogger(HelloControllerBearer.class);
-
-	@ConnectMapping
-	public void setup(@Payload String payload, @Headers Map<String, Object> headers) {
-		log.info("setup:payload={}", payload);
-		log.info("setup:headers={}", headers);
-	}
 
 	@MessageMapping("hello")
-	public String hello(@AuthenticationPrincipal Jwt jwt, @Payload String payload, @Headers Map<String, Object> headers) {
-		log.info("hello:payload={}", payload);
-		log.info("hello:headers={}", headers);
+	public String hello(@AuthenticationPrincipal Jwt jwt, @Payload String payload) {
 		return "Hello " + payload + ", " + jwt.getClaimAsString("email") + "!";
 	}
 }
